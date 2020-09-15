7cb6f11f8bb88d7fd6bc0c9edee868d67c16ce57
tushar555 <tusharsaindane02@gmail.com>
2020-09-15 20:27:07 +0530

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { exception } from 'console';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
+  });
+
+  //Checked if Component has intiated and app has started.
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    let component = TestBed.createComponent(AppComponent);
+    let app = component.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'multicloud-assessment'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('multicloud-assessment');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('multicloud-assessment app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface CardDetails {
+  id: number,
+  title: string,
+  content: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'multicloud-assessment';
+  title: string;
+  content: string;
+  cardDetails: CardDetails[] = [];
+
+  addCard(): void {
+    this.cardDetails.push({ id: Math.random(), title: this.title || 'Card', content: this.content || 'This is card content' });
+  }
+  removeCard(card: CardDetails): void {
+    this.cardDetails = this.cardDetails.filter(obj => obj.id !== card.id);
+  }
 }
